eaf62eb76b8d9aae9dd23ea8dafc7066e3dcce4f
gutierrezp22 <pabloexegutierrez@gmail.com>
2025-10-13 22:46:51 -0300

    Update descripcion field in ActividadReservada model from CharField to TextField to allow longer descriptions up to 4000 characters.

--- backend/models/actividad_reservada.py
+++ backend/models/actividad_reservada.py
@@ -5,7 +5,7 @@ from .estandar import Estandar
 
 class ActividadReservada(models.Model):
     estandar = models.ForeignKey(Estandar, on_delete=models.PROTECT)
-    descripcion = models.CharField(max_length=255)
+    descripcion = models.TextField(max_length=4000)
 
     def __str__(self):
         return self.descripcion
