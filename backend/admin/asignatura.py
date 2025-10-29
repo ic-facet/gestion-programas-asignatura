bfcb0abb7afbcb7f48b38ba29188429a52085965
Pablo Gutierrez <pgutierrez@cajapopular.gov.ar>
2025-10-29 16:39:23 -0300

    Add AsignaturaAdminForm to set default values for hour fields in admin
    - Introduced AsignaturaAdminForm to initialize specific hour fields with a default value of 0 for new instances.
    - Updated AdminAsignatura to use the new form for better management of hour fields in the admin interface.

--- backend/admin/asignatura.py
+++ backend/admin/asignatura.py
@@ -1,7 +1,32 @@
 from django.contrib import admin
+from django import forms
+
+
+class AsignaturaAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Campos de horas con valor por defecto 0
+        campos_horas = [
+            'total_teoria_presencial',
+            'total_practica_presencial',
+            'total_teorico_practico_presencial',
+            'total_lab_presencial',
+            'total_teoria_remoto',
+            'total_practica_remoto',
+            'total_teorico_practico_remoto',
+            'total_lab_remoto',
+            'horas_evaluacion',
+        ]
+
+        # Solo establecer el valor inicial si el objeto es nuevo
+        if not self.instance.pk:
+            for campo in campos_horas:
+                if campo in self.fields:
+                    self.fields[campo].initial = 0
 
 
 class AdminAsignatura(admin.ModelAdmin):
+    form = AsignaturaAdminForm
     list_display = ["codigo", "denominacion", "horas_totales_clases", "carga_total", "metodologia"]
     list_display_links = ["codigo", "denominacion"]
     search_fields = ["codigo", "denominacion"]
