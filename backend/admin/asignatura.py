8d7d28678b63c3c113c4659442cb1ffb14fb94f2
gutierrezp22 <pabloexegutierrez@gmail.com>
2025-10-28 22:51:46 -0300

    Refactor assignment model fields and update related views and templates
    - Renamed fields in the Asignatura model from 'semanal' to 'total' for clarity in representing total hours.
    - Updated related views, serializers, and templates to reflect the new field names.
    - Adjusted test cases to ensure consistency with the updated model structure.

--- backend/admin/asignatura.py
+++ backend/admin/asignatura.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 
 class AdminAsignatura(admin.ModelAdmin):
-    list_display = ["codigo", "denominacion", "horas_semanales_clases", "carga_total", "metodologia"]
+    list_display = ["codigo", "denominacion", "horas_totales_clases", "carga_total", "metodologia"]
     list_display_links = ["codigo", "denominacion"]
     search_fields = ["codigo", "denominacion"]
     list_filter = ["metodologia", "bloque_curricular"]
